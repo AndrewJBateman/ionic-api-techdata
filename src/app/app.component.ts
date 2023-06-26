351b7c006ea37b16d1b3b116035d1b4f08dcbdbc
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-26 13:14:02 +0200

    refactor: remove unrequired code, linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  template: `<ion-app>
+  <ion-router-outlet></ion-router-outlet>
+</ion-app>`
 })
 export class AppComponent {
   constructor() {}
